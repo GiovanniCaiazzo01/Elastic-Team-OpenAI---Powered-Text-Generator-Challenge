5e01ef60e38c46fb3067e72013b728a90e24cd79
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-06-25 15:34:33 +0200

    feat: layouts Component <Container />

--- client/src/layouts/Container/style.jsx
+++ client/src/layouts/Container/style.jsx
@@ -1,19 +1,56 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  ${(props) => props.theme && console.log(props.theme)}
-`;
+  ${(props) => props.width && `width: ${props.theme.sizes.px[props.width]};`}
+  ${(props) => props.height && `height: ${props.theme.sizes.px[props.height]};`}
 
-// height
-// backgroundColor
-// borderRadius
-// padding
-// paddingLeft
-// paddingRight
-// paddingTop
-// paddingBottom
-// margin
-// marginLeft
-// marginRight
-// marginTop
-// marginBottom
+  // PADDING SECTION
+  ${(props) =>
+    props.padding && `padding: ${props.theme.sizes.px[props.padding]}`}
+    ${(props) =>
+    props.paddingTop &&
+    !props.padding &&
+    `padding-top: ${props.theme.sizes.px[props.paddingTop]};`}
+${(props) =>
+    props.paddingBottom &&
+    !props.padding &&
+    `padding-bottom: ${props.theme.sizes.px[props.paddingBottom]};`}
+${(props) =>
+    props.paddingLeft &&
+    !props.padding &&
+    `padding-left: ${props.theme.sizes.px[props.paddingLeft]};`}
+${(props) =>
+    props.paddingRight &&
+    !props.padding &&
+    `padding-right: ${props.theme.sizes.px[props.paddingRight]};`}
+
+    // MARGIN SECTION
+    ${(props) =>
+    props.margin && `margin: ${props.theme.sizes.px[props.margin]}`}
+        ${(props) =>
+    props.marginTop &&
+    !props.margin &&
+    `margin-top: ${props.theme.sizes.px[props.marginTop]};`}
+    ${(props) =>
+    props.marginBottom &&
+    !props.margin &&
+    `margin-bottom: ${props.theme.sizes.px[props.marginBottom]};`}
+    ${(props) =>
+    props.marginLeft &&
+    !props.margin &&
+    `margin-left: ${props.theme.sizes.px[props.marginLeft]};`}
+    ${(props) =>
+    props.marginRight &&
+    !props.margin &&
+    `margin-right: ${props.theme.sizes.px[props.marginRight]};`}
+
+    //BORDER SECTION
+    ${(props) =>
+    props.borderRadius &&
+    `border-radius: ${props.theme.sizes.px[props.borderRadius]}`}
+
+    //BACKGROND SECTION
+    ${(props) =>
+    props.backgroundColor &&
+    `background-color: ${props.theme.color[props.backgroundColor]}`}
+`;
