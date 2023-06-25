019eb250e80042bf0fc1a1a362dbd44c25b6f2fc
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-06-25 14:34:08 +0200

    feat: made the theme for the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-componets";
 import MainRoutes from "./routes/MainRoutes";
+import { theme } from "./themes/";
 import "./App.css";
+
 const App = () => {
   return (
     <BrowserRouter>
-      <MainRoutes />
+      <ThemeProvider theme={theme}>
+        <MainRoutes />
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
---
+++ client/src/themes/theme.js
@@ -0,0 +1,26 @@
+export const theme = {
+  colors: {
+    primary: "#FF007A",
+    black: "#000000",
+    darkGray: "#FF007A",
+    backGround: "#FFFFFF",
+    BrandGreen: "#46B455",
+  },
+  fonts: {
+    body: "Arial, sans-serif",
+    heading: "Helvetica, sans-serif",
+  },
+  breakpoints: {
+    mobile: "576px",
+    tablet: "768px",
+    desktop: "992px",
+    largeDesktop: "1200px",
+  },
+  sizes: {
+    px: [
+      2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38,
+      40,
+    ],
+    rem: [1, 2, 3],
+  },
+};
