9b3334ee1e45495f59f30fcbdf21c52dd7a3a0f0
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-07-03 02:34:11 +0200

    chore: added new dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./themes/theme";
 import MainRoutes from "./routes/MainRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <ToastContainer />
         <MainRoutes />
       </ThemeProvider>
     </BrowserRouter>
