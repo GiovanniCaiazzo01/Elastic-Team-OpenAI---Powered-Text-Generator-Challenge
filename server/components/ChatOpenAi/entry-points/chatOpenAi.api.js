9951077e0dbf0d67d9f024a5eb9de1251f74c044
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-07-07 10:50:57 +0200

    chore: removed log

--- server/components/ChatOpenAi/entry-points/chatOpenAi.api.js
+++ server/components/ChatOpenAi/entry-points/chatOpenAi.api.js
@@ -4,7 +4,6 @@ const router = Router();
 
 router.post("/", async (req, res) => {
   const { question } = req.body;
-  console.log("quante volte vengo chiamato");
 
   const paylaod = {
     question,
